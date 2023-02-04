405ed0cdc11c0962c61a22db81e61774d1ba9d11
Cristian Hernandez <cmhrlxl@gmail.com>
2023-02-04 11:41:29 +0000

    Debugging middlewares v.1.4

--- src/middlewares/require-auth.ts
+++ src/middlewares/require-auth.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import { NotAuthorizedError } from '../errors/NotAuthorizedError';
 
 export const requireAuth = (req: Request, res: Response, next: NextFunction) => {
+    console.log('require-auth: ', req.currentUser)
     if(!req.currentUser){
         throw new NotAuthorizedError();
     }
