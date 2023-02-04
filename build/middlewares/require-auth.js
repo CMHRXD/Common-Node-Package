c5535d0a5c13274a91bbbaae47f48a7891143d2f
Cristian Hernandez <cmhrlxl@gmail.com>
2023-02-04 11:48:22 +0000

    Debugging middlewares v.1.5

--- build/middlewares/require-auth.js
+++ build/middlewares/require-auth.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.requireAuth = void 0;
 const NotAuthorizedError_1 = require("../errors/NotAuthorizedError");
 const requireAuth = (req, res, next) => {
+    console.log(req);
     console.log('require-auth: ', req.currentUser);
     if (!req.currentUser) {
         throw new NotAuthorizedError_1.NotAuthorizedError();
