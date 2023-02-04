ce08a1c2434d0d5dfcf773c0bb28f4f81a750964
Cristian Hernandez <cmhrlxl@gmail.com>
2023-02-04 11:14:34 +0000

    Debugging middlewares v.1.0

--- src/middlewares/tokenMiddleware.ts
+++ src/middlewares/tokenMiddleware.ts
@@ -22,6 +22,7 @@ export const tokenMiddleware = (req: Request, res: Response, next: NextFunction)
         next();
     }
     const token = req.session?.token;
+    console.log(req)
     console.log('session: ', req.session)
     console.log('token: ', token)
     try {
