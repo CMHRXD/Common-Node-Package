7df198a5976f07d94cb72997e0549b967a3c4769
Cristian Hernandez <cmhrlxl@gmail.com>
2023-03-05 22:16:09 +0000

    Adding ENV checks

--- src/helpers/ENV_Check.ts
+++ src/helpers/ENV_Check.ts
@@ -1,5 +1,14 @@
-export function envCheck (){
-    if(!process.env.MONGO_URI || !process.env.JWT_SECRET) {
-        throw new Error('ENV variables not found')
-    }
+export function envCheck() {
+  if (!process.env.MONGO_URI || !process.env.JWT_SECRET) {
+    throw new Error("ENV variables not found");
+  }
+  if (!process.env.CLUSTER_ID) {
+    throw new Error("NATS CLUSTER_ID not found");
+  }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS CLIENT_ID not found");
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS URL not found");
+  }
 }
