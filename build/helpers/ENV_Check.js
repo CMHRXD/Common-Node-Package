8c082acad41125f431d65e11da0a77245a324e29
Cristian Hernandez <cmhrlxl@gmail.com>
2023-03-05 22:21:52 +0000

    Deleting ENV checks

--- build/helpers/ENV_Check.js
+++ build/helpers/ENV_Check.js
@@ -5,14 +5,5 @@ function envCheck() {
     if (!process.env.MONGO_URI || !process.env.JWT_SECRET) {
         throw new Error("ENV variables not found");
     }
-    if (!process.env.CLUSTER_ID) {
-        throw new Error("NATS CLUSTER_ID not found");
-    }
-    if (!process.env.NATS_CLIENT_ID) {
-        throw new Error("NATS CLIENT_ID not found");
-    }
-    if (!process.env.NATS_URL) {
-        throw new Error("NATS URL not found");
-    }
 }
 exports.envCheck = envCheck;
